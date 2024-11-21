1608c35721d97b1e5f2778863c3acf6b163f6b20
Jiyu Shao <jiyu.shao@gmail.com>
2024-11-21 13:49:51 +0800

    feat(project): init project

---
+++ CONTRIBUTING.md
@@ -0,0 +1,21 @@
+# CONTRIBUTING
+
+## 快速开始
+
+### 安装依赖&启动编译服务
+
+```bash
+# 安装依赖
+pnpm install
+
+# 开启编译服务，打开 http://localhost:3000 即可进行调试
+pnpm start
+```
+
+## 打包&发布
+
+### 构建
+
+```bash
+pnpm run build
+```
---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+
+export default [
+  {
+    ignores: [
+      // Ignore dotfiles
+      '.*.js',
+      'node_modules/',
+      'dist/',
+      'assets/',
+    ],
+  },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+  pluginReactConfig,
+];
